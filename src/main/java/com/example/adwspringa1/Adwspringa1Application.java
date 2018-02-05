0c8b9e089810839ba690ac1c2a26907385212a8c
Sniperzown <Sniperzown@hotmail.co.uk>
2018-02-05 16:45:35 +0000

    Tester

---
+++ src/main/java/com/example/adwspringa1/Adwspringa1Application.java
@@ -0,0 +1,12 @@
+package com.example.adwspringa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adwspringa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adwspringa1Application.class, args);
+	}
+}
